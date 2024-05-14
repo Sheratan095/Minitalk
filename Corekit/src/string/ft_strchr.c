70d51ae27494bcb3617c702e5311261caf866f26
Marco Ceccarelli <maceccar@c2r1p10.42firenze.it>
2024-05-14 15:39:30 +0200

    Corekit static

---
+++ Corekit/src/string/ft_strchr.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maceccar <maceccar@student.42firenze.it>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/16 18:40:17 by maceccar          #+#    #+#             */
+/*   Updated: 2024/04/24 22:35:04 by maceccar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strchr(const char *str, int c)
+{
+	char	check;
+
+	check = (char)c;
+	while (*str)
+	{
+		if (*str == check)
+			return ((char *)str);
+		str++;
+	}
+	if (*str == check)
+		return ((char *)str);
+	return (NULL);
+}
